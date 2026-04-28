d0890ea37c5161f350a41ec558ca365c1a67991f
Elias <mcanimations056@gmail.com>
2026-04-28 10:31:53 +0200

    Update fix.js to use ES modules

--- fix.js
+++ fix.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import fs from 'fs';
 const path = 'src/server/functions/tickets.ts';
 let content = fs.readFileSync(path, 'utf8');
 content = content.split('\\`').join('`').split('\\$').join('$');
